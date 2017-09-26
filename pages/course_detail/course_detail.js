74f96abfb3ee3999c1bdd82e15be5a78b0ba2695
reddwen <422958551@qq.com>
2017-09-26 22:19:12 +0800

    新增了course_detail文件夹及内容

---
+++ pages/course_detail/course_detail.js
@@ -0,0 +1,72 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log("生命周期函数--监听页面加载")
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    console.log("生命周期函数--监听页面初次渲染完成")
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    console.log("生命周期函数--监听页面显示")
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    console.log("生命周期函数--监听页面隐藏")
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    console.log("生命周期函数--监听页面卸载")
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    console.log("页面相关事件处理函数--监听用户下拉动作")
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    console.log("页面上拉触底事件的处理函数")
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    console.log("用户点击右上角分享")
+
+  }
+})
