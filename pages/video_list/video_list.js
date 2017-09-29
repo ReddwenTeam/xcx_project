a3b0b4650b45b237f033621c3bff4b8c441dbcd1
reddwen <422958551@qq.com>
2017-09-29 17:26:08 +0800

    新增信息审核，申报记录，招生详情

---
+++ pages/video_list/video_list.js
@@ -0,0 +1,100 @@
+var VIDEO_LIST = [
+  {
+    title: "他们有一种魔力，让我情不自禁",
+    sub_title: "《光谷外校欢迎你》MV",
+    tag: "独特",
+    duration: "3分45秒",
+    id: 1,
+    poster_src: "../../temp/index_5.png"
+  },
+  {
+    title:"风华正茂 海外扬帆",
+    sub_title:"中学2016年迎新春音乐会",
+    tag:"活力",
+    duration:"120分45秒",
+    id:2,
+    poster_src:"../../temp/index_2.png"
+  },
+  {
+    title: "好学生必是好创意者",
+    sub_title: "创意无处不在宣传片",
+    tag: "创意",
+    duration: "10分45秒",
+    id: 3,
+    poster_src: "../../temp/index_3.png"
+  },
+  {
+    title: "全方位呵护您孩子的营养健康",
+    sub_title: "生活专家 每周食谱",
+    tag: "健康",
+    duration: "5分45秒",
+    id: 4,
+    poster_src: "../../temp/index_4.png"
+  },
+]
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    video_list: VIDEO_LIST
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
