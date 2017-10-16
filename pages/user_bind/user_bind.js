2902389fcebfa51bff070bdc1cb74504924afb34
selinana <714804010@qq.com>
2017-10-16 22:14:42 +0800

    绑定页面

--- pages/user_bind/user_bind.js
+++ pages/user_bind/user_bind.js
@@ -1,8 +1,27 @@
 Page({
   data: {
-
+    pageNum:0,
+    tempName:"jiazhang"
   },
   onLoad: function (param) {
     console.log(param)
+    this.setData({
+      avatarUrl: param.avatarUrl
+    });
   },
+  tapCheck: function (event) {
+    var dataSet = event.currentTarget.dataset;
+    this.setData({
+      pageNum: dataSet.pageNum
+    });
+    if (dataSet.pageNum == 0){
+      this.setData({
+        tempName: "jiazhang"
+      });
+    }else{
+      this.setData({
+        tempName: "laoshi"
+      })
+    }
+  }
 })
